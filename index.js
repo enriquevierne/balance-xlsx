66c9b8609eaedd23d67e41e36c2c933d51014ed4
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2024-05-15 12:43:28 -0300

    add script to return requet

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const xlsx = require("xlsx");
 const express = require("express");
 
-const file = "./Balancete.xlsx";
+const file = "./Balancetes_out.xlsx";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const wb = xlsx.readFile(file)
-  const ws = wb.Sheets["Relatório"]
-  const data = xlsx.utils.sheet_to_json(ws)
+  const array = [];
+  const wb = xlsx.readFile(file);
+  const ws = wb.Sheets["Relatório"];
+  const data = xlsx.utils.sheet_to_json(ws);
+  let total = 0
 
-  res.send(data)
+  data.map(item => {
+    if(item.__EMPTY && item.__EMPTY_4 && item.__EMPTY_15){
+      if(!isNaN(item.__EMPTY_15)) total = total + item.__EMPTY_15
+      array.push({
+        'categoria': item.__EMPTY,
+        'item' : item.__EMPTY_4,
+        'price' : item.__EMPTY_15,
+        
+                })
+                array.push({'total' : total})
+              }
+            },
+          ) 
+          
+
+
+
+  res.send(array);
 });
 
 app.listen(port, () => {
